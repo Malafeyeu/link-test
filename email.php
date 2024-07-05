9ceceb214dac9bc4e88bcc0468a78d9c3514ce30
MOLOFEEVilya <m_molo@mail.ru>
2024-07-05 20:14:57 +0300

    check

--- email.php
+++ email.php
@@ -1,38 +1,39 @@
 <?php
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
+echo "Hello, world!";
+// use PHPMailer\PHPMailer\PHPMailer;
+// use PHPMailer\PHPMailer\Exception;
 
-require './PHPMailer/src/Exception.php';
-require './PHPMailer/src/PHPMailer.php';
-require './PHPMailer/src/SMTP.php';
+// require './PHPMailer/src/Exception.php';
+// require './PHPMailer/src/PHPMailer.php';
+// require './PHPMailer/src/SMTP.php';
 
-if (isset($_POST['send'])) {
-  $name = htmlentities($_POST['name']);
-  $phone = htmlentities($_POST['phone']);
+// if (isset($_POST['send'])) {
+//   $name = htmlentities($_POST['name']);
+//   $phone = htmlentities($_POST['phone']);
   
-  $mail = new PHPMailer(true);
-  $mail->isSMTP();
-  $mail->Host = 'smtp.gmail.com';
-  $mail->SMTPAuth = true;
-  $mail->Username = 'akts.nagato@gmail.com';
-  $mail->Password = 'swfz dgup opoq fgze';
-  $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+//   $mail = new PHPMailer(true);
+//   $mail->isSMTP();
+//   $mail->Host = 'smtp.gmail.com';
+//   $mail->SMTPAuth = true;
+//   $mail->Username = 'akts.nagato@gmail.com';
+//   $mail->Password = 'swfz dgup opoq fgze';
+//   $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
   
-  $mail->Port = 465;
-  $mail->isHTML(true);
-  $mail->setFrom('akts.nagato@gmail.com', $name);
-  $mail->addAddress('akts.nagato@gmail.com');
-  $mail->Subject = 'Contact Form';
-  $mail->Body = '<h3>Hi there! You got a new enquiry</h3>
-    <h4>Name: '.$name.'</h4>
-    <h4>Phone: '.$phone.'</h4>
-  ';
-  if ($mail->send()) {
-    return "Email not send";
-  } else {
-    return "success";
-  } 
+//   $mail->Port = 465;
+//   $mail->isHTML(true);
+//   $mail->setFrom('akts.nagato@gmail.com', $name);
+//   $mail->addAddress('akts.nagato@gmail.com');
+//   $mail->Subject = 'Contact Form';
+//   $mail->Body = '<h3>Hi there! You got a new enquiry</h3>
+//     <h4>Name: '.$name.'</h4>
+//     <h4>Phone: '.$phone.'</h4>
+//   ';
+//   if ($mail->send()) {
+//     return "Email not send";
+//   } else {
+//     return "success";
+//   } 
   
-  header('Location: ./response.html');
-}
+//   header('Location: ./response.html');
+// }
 ?>
